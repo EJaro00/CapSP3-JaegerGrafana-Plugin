5f94dbfc30a30ce6c4594e79e374998f3212fa4a
EJaro00 <eric_jaroszewski1@alumni.baylor.edu>
2023-02-08 11:23:12 -0600

    Added Maven project files

---
+++ src/test/java/capstone2023/opentelemetry/AppTest.java
@@ -0,0 +1,20 @@
+package capstone2023.opentelemetry;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
