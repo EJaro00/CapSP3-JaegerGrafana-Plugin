f2856aae8731be331835ed3d74a6c3d2a725332c
Hou <Maiqi_Hou1@baylor.edu>
2023-02-28 15:14:10 -0600

    Read_Datasource_connection

--- test-plugin/src/SimplePanel.tsx
+++ test-plugin/src/SimplePanel.tsx
@@ -1,105 +1,136 @@
 import React from 'react';
 import { PanelProps} from '@grafana/data';
 import { SimpleOptions } from 'types';
-import { useTheme } from '@grafana/ui';
-import * as d3 from 'd3';
-import { SimulationNodeDatum } from 'd3';
+import * as d3 from "d3"
+import { SimulationNodeDatum} from 'd3';
 
 interface Props extends PanelProps<SimpleOptions> {}
 
 
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
-  const theme = useTheme();
-  let color: string;
-  switch (options.color) {
-    case 'red':
-      color = theme.palette.redBase;
-      break;
-    case 'green':
-      color = theme.palette.greenBase;
-      break;
-    case 'blue':
-      color = theme.palette.blue95;
-      break;
+  let nodes = [] as any []
+  let links = [] as any []
+  function add (){
+    const index = data.series.map((series) => series.fields.find((field) => field.name === 'Index'));
+    const name = data.series.map((series) => series.fields.find((field) => field.name === 'Name'));
+    const source = data.series.map((series) => series.fields.find((field) => field.name === 'Source'));
+    let array = index[0]?.values
+    for(let i = 0; i < (array?.length as number) ; i++){
+      let p = (array?.get(i) as number) 
+      let n = name[0]?.values.get(i)
+      let s = source[0]?.values.get(i)
+      if(p !== null && n !== null){
+        nodes.push({name: n})
+        links.push({source: s, target: p})
+      }
+    }
+    for(let i = 0; i < links.length; i++){
+      console.log(links[i])
+    }
   }
-  var nodes = [{name: 'DBNode',}, 
-                {name: 'tsDB'}, 
-                {name: 'nacoDB'},
-                {name: 'tsDB-mysql0'},
-                {name: 'tsDB-mysql1'},
-                {name: 'tsDB-mysql2'},
-                {name: 'nacoDB-mysql0'},
-                {name: 'nacoDB-mysql1'},
-                {name: 'nacoDB-mysql2'},
-                {name: 'ui-service'}
-              ]
+  
+
   function updateNodes() {
-    var u = d3.select('.nodes')
+    d3.select('.nodes')
+      .selectAll('circle')
+      .data(nodes)
+      .join('circle')
+      .attr('cx', function(d:any){return d.x})
+      .attr('cy', function(d:any){return d.y})
+      .attr('r', 20)
+      .attr('opacity', 0.5)
+      .attr('fill', "#5DADE2");
+
+    d3.select('.nodes')
       .selectAll('text')
       .data(nodes)
       .join('text')
       .text(function(d) {
         return d.name
       })
-      .attr('x', function(d:any) {
-        return d.x
-      })
-      .attr('y', function(d:any) {
-        return d.y
-      })
-      .attr('dy', function(d) {
-        return 10
-      })
-      .attr('fill',color);
-    return u
-}
-
-var links = [
-  {source: 0, target: 1},
-  {source: 0, target: 2},
-  {source: 1, target: 3},
-  {source: 1, target: 4},
-  {source: 1, target: 5},
-  {source: 2, target: 6},
-  {source: 2, target: 7},
-  {source: 2, target: 8},
-  {source: 0, target: 9}
-
-]
+      .attr('x', function(d: any) {return d.x})
+      .attr('y', function(d: any) {return d.y})
+      .attr('dx', function(d){return -15})
+      .attr('dy', function(d) {return -15})
+      .attr('fill',"#58D68D");
+  }
   
-function updateLinks() {
-  var u = d3.select('.links')
-    .selectAll('line')
-    .data(links)
-    .join('line')
-    .attr('x1', function(d:any) {
-      return d.source.x
-    })
-    .attr('y1', function(d:any) {
-      return d.source.y
-    })
-    .attr('x2', function(d:any) {
-      return d.target.x
-    })
-    .attr('y2', function(d:any) {
-      return d.target.y
-    })
-    .attr('stroke','#ccc');
-  return u
-}
+  function updateLinks() {
+    d3.select('.links')
+      .selectAll('path')
+      .data(links)
+      .join('path')
+      .attr('d', (d: any) => {
+        const dx = d.target.x - d.source.x;
+        const dy = d.target.y - d.source.y;
+        const l = Math.sqrt(dx * dx + dy * dy);
+        const sx = d.source.x + (dx * 20) / l;
+        const sy = d.source.y + (dy * 20) / l;
+        const tx = d.target.x - (dx * 20) / l;
+        const ty = d.target.y - (dy * 20) / l;
+        const arrowSize = 8;
+        const offsetX = (dx * arrowSize) / l;
+        const offsetY = (dy * arrowSize) / l;
+        return `M ${sx},${sy} 
+                L ${tx},${ty} 
+                M ${tx - offsetX - offsetY},${ty - offsetY + offsetX} 
+                L ${tx},${ty} L ${tx - offsetX + offsetY},${ty - offsetY - offsetX} Z`;
+      })
+      .attr('fill', "#8E44AD")
 
-function ticked() {
-  updateLinks()
-  updateNodes();
-}
+      d3.select('.links')
+      .selectAll('line')
+      .data(links)
+      .join('line')
+      .attr('x1', function(d: any) {
+        const dx = d.target.x - d.source.x;
+        const dy = d.target.y - d.source.y;
+        const l = Math.sqrt(dx * dx + dy * dy);
+        const sx = d.source.x + (dx * 20) / l;
+        //const sy = d.source.y + (dy * 20) / l;
+        return sx;
+      })
+      .attr('y1', function(d: any) {
+        const dx = d.target.x - d.source.x;
+        const dy = d.target.y - d.source.y;
+        const l = Math.sqrt(dx * dx + dy * dy);
+        //const sx = d.source.x + (dx * 20) / l;
+        const sy = d.source.y + (dy * 20) / l;
+        return sy;
+      })
+      .attr('x2', function(d: any) {
+        const dx = d.target.x - d.source.x;
+        const dy = d.target.y - d.source.y;
+        const l = Math.sqrt(dx * dx + dy * dy);
+        const tx = d.target.x - (dx * 20) / l;
+        //const ty = d.target.y - (dy * 20) / l;
+        return tx;
+      })
+      .attr('y2', function(d: any) {
+        const dx = d.target.x - d.source.x;
+        const dy = d.target.y - d.source.y;
+        const l = Math.sqrt(dx * dx + dy * dy);
+        //const tx = d.target.x - (dx * 20) / l;
+        const ty = d.target.y - (dy * 20) / l;
+        return ty;
+      })
+      .attr('stroke', '#ccc');
+  }
 
-var simulation = d3.forceSimulation(nodes as SimulationNodeDatum[]).force('charge', d3.forceManyBody().strength(-3550))
-	.force('center', d3.forceCenter(width / 2, height / 2))
-	.force('link', d3.forceLink().links(links))
-
-	.on('tick', ticked);
-  simulation.restart
+  add()
+  function ticked() {
+    updateLinks();
+    updateNodes();
+  }
   
+  let simulate = d3.forceSimulation(nodes as SimulationNodeDatum[])
+    .force('charge', d3.forceManyBody().strength(-1500))
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .force('link', d3.forceLink().links(links))
+    .on('tick', ticked);
+  simulate.stop
+  
+
   return (
     <svg width={width} height={height}>
       <g className="links"></g>
