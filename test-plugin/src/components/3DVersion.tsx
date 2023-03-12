052bdd2f20220869bd971814670e328cdeb5d44a
Hou <Maiqi_Hou1@baylor.edu>
2023-03-12 14:45:32 -0500

    updated3D]

--- test-plugin/src/components/3DVersion.tsx
+++ test-plugin/src/components/3DVersion.tsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import {PanelData} from '@grafana/data';
-import ForceGraph3D, { ForceGraphMethods } from 'react-force-graph-3d';
+import ForceGraph3D, { ForceGraphMethods} from 'react-force-graph-3d';
+import SpriteText from 'three-spritetext';
 
 type Props = {
     data: PanelData;
@@ -21,8 +22,8 @@ const graph: React.FC<Props> = ({data, width, height}) =>{
           let n = name[0]?.values.get(i)
           let s = source[0]?.values.get(i)
           if(p !== null && n !== null){
-            if(p !== n){
-                nodes.push({'id': p, name: n})
+            nodes.push({'id': p, 'name': n})
+            if(i > 0){
                 links.push({'source': s, 'target': p})
             }
           }
@@ -34,15 +35,26 @@ const graph: React.FC<Props> = ({data, width, height}) =>{
     const reference = useRef<ForceGraphMethods>();
     return (
         <ForceGraph3D
+
             ref={reference}
             graphData={{nodes,links}}
             width={width}
             height={height}
-            linkDirectionalArrowColor={() => 'rgba(0,255,255,1)'}
-            linkColor={() => 'rgba(0,255,255,1)'}
+            nodeLabel={'id'}
+            linkDirectionalParticles={2}
+            linkDirectionalParticleWidth={3}
+            linkDirectionalParticleColor={()=>'rgba(51,255,51,1)'}
+            linkColor={() => 'rgba(224,224,224,1)'}
             linkWidth = {3}
-            nodeColor = {() => 'rgba(0,204,102,1)'}
-            nodeLabel = {nodes.name}
+            nodeColor = {() => 'rgba(0,128,255,1)'}
+            nodeThreeObjectExtend={true}
+            nodeThreeObject={node => {
+                const sprite = new SpriteText(node.name);
+                sprite.color = 'rgba(255,255,255,1)'
+                sprite.textHeight = 5
+                sprite.position.y = 8
+                return sprite;
+              }}
         />
     );
 }
