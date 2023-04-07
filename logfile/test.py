30c674f900583afaae22781f7b381edc91bbd713
Hou <Maiqi_Hou1@baylor.edu>
2023-04-07 17:25:31 -0500

    update trace log file

--- logfile/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-import logging
-
-logging.basicConfig(filename='train-ticket.log', level=logging.DEBUG)
-service_name = 'ts-user-service'
-url = f'http://192.168.3.203:5000/api/traces?service={service_name}'
-response = requests.get(url)
-logging.debug(response.content.decode())
