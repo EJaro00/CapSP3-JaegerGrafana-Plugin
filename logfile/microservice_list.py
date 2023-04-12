b9993428fa82ef8f6d0b518c7d701617ea8608d2
Maiqi Hou <70655340+HLeoF@users.noreply.github.com>
2023-04-12 14:42:10 -0500

    Add files via upload

---
+++ logfile/microservice_list.py
@@ -0,0 +1,9 @@
+list = ['ts-admin-basic-info-service','ts-admin-order-service','ts-admin-route-service',
+        'ts-admin-travel-service','ts-admin-user-service', 'ts-assurance-service',
+        'ts-auth-service','ts-basic-service','ts-cancel-service','ts-config-service',
+        'ts-consign-price-service','ts-consign-service','ts-contacts-service','ts-execute-service',
+        'ts-food-map-service','ts-food-service','ts-inside-payment-service','ts-notification-service',
+        'ts-order-other-service','ts-order-service','ts-payment-service','ts-preserve-other-service',
+        'ts-preserve-service','ts-price-service','ts-rebook-service','ts-route-plan-service','ts-route-service',
+        'ts-seat-service','ts-security-service','ts-station-service','ts-ticketinfo-service','ts-train-service',
+        'ts-travel-plan-service','ts-travel-service','ts-travel2-service','ts-user-service','ts-verification-code-service']
