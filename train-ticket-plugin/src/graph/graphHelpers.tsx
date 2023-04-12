5c06bc3f503e7c20edb14686d9b889b22301d0a4
EJaro00 <43527083+EJaro00@users.noreply.github.com>
2023-04-12 16:34:20 -0500

    Update graphHelpers.tsx
    Added in functions to get indegree and outdegree of nodes, which are then used to find Bottleneck and Nanoservice antipatterns in a graph

--- train-ticket-plugin/src/graph/graphHelpers.tsx
+++ train-ticket-plugin/src/graph/graphHelpers.tsx
@@ -25,7 +25,7 @@ function dfsSCC(vertices: any[], edges: any[], visited: boolean[], currentNode: 
 * @param edges Array of all vertex-to-vertex edges in our graph
 * @returns An array of arrays, each inner array containing the vertices that form a strongly connected component
 */
-export function findSCCs(vertices: any[], edges: any[]): number[][] {
+function findSCCs(vertices: any[], edges: any[]): number[][] {
   const visited: boolean[] = new Array(vertices.length).fill(false);
   const stack: any[] = [];
   const sccs: number[][] = [];
@@ -48,6 +48,33 @@ export function findSCCs(vertices: any[], edges: any[]): number[][] {
   return sccs;
 }
 
+/**
+ * Returns the indegree of a node in a graph
+ * @param node The node we are investigating
+ * @param edges The array of all edges in the graph
+ * @returns The number of edges pointing towards this node (indegree)
+ */
+function getDegreeIn(node: any, edges: any[]): number {
+  let retVal: number = 0;
+
+  retVal = edges.filter(link => link.target === node.name).length;
+
+  return retVal;
+}
+
+/**
+ * Returns the outdegree of a node in a graph
+ * @param node The node we are investigating
+ * @param edges The array of all edges in the graph
+ * @returns The number of edges pointing away from this node (outdegree)
+ */
+function getDegreeOut(node: any, edges: any[]): number {
+  let retVal: number = 0;
+
+  retVal = edges.filter(link => link.source === node.name).length;
+
+  return retVal;
+}
 
 /**
  * Finds all nodes that make up cycles in a graph
@@ -57,20 +84,61 @@ export function findSCCs(vertices: any[], edges: any[]): number[][] {
  */
 export function findCycles(vertices: any[], edges: any[]): boolean[]{
 
-    const retVal: boolean[] = new Array(vertices.length).fill(false);
-    let sccs: number[][] = findSCCs(vertices, edges);
-    let index : number = 0;
+  const retVal: boolean[] = new Array(vertices.length).fill(false);
+  let sccs: number[][] = findSCCs(vertices, edges);
+  let index : number = 0;
 
-    for(let i = 0; i < sccs.length; i++){
-        //If the SCC contains more than one node, every node in the SCC is part of a cycle.
-        if(sccs[i].length > 1){
-            for(let k = 0; k < sccs[i].length; k++){
-                index = sccs[i][k];
-                retVal[index] = true;
-            }
-        }
+  for(let i = 0; i < sccs.length; i++){
+      //If the SCC contains more than one node, every node in the SCC is part of a cycle.
+      if(sccs[i].length > 1){
+          for(let k = 0; k < sccs[i].length; k++){
+              index = sccs[i][k];
+              retVal[index] = true;
+          }
+      }
+  }
+
+  return retVal;
+
+}
+
+/**
+ * Finds all bottleneck vertices in a graph according to a threshold
+ * @param vertices Array of all vertices in our graph
+ * @param edges Array of all vertex-to-vertex edges in our graph
+ * @param threshold The user-defined threshold for how many edges create a bottleneck
+ * @returns An array containing a boolean for each vertex - true if it's a bottleneck, false otherwise
+ */
+export function findBottlenecks(vertices: any[], edges: any[], threshold: number): boolean[] {
+
+  const retVal: boolean[] = new Array(vertices.length).fill(false);
+
+  for(let i = 0; i < vertices.length; i++){
+    if(getDegreeIn(vertices[i], edges) > threshold){
+      retVal[i] = true;
     }
+  }
 
-    return retVal;
+  return retVal;
 
+}
+
+/**
+ * Finds all nanoservice vertices in a graph according to a threshold
+ * @param vertices Array of all vertices in our graph
+ * @param edges Array of all vertex-to-vertex edges in our graph
+ * @param threshold The user-defined threshold for how many edges create a nanoservice
+ * @returns An array containing a boolean for each vertex - true if it's a nanoservice, false otherwise
+ */
+export function findNanoservices(vertices: any[], edges: any[], threshold: number): boolean[] {
+  
+  const retVal: boolean[] = new Array(vertices.length).fill(false);
+
+  for(let i = 0; i < vertices.length; i++){
+    if(getDegreeOut(vertices[i], edges) > threshold){
+      retVal[i] = true;
+    }
+  }
+
+  return retVal;
 }
